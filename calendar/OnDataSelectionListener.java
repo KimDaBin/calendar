22af6483d0d7c4c815573139fe97f58d5c3052e1
KimDaBin <33649908+KimDaBin@users.noreply.github.com>
2018-07-18 21:58:51 +0900

    Delete OnDataSelectionListener.java

--- calendar/OnDataSelectionListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.caucse.calendar;
-
-import android.view.View;
-import android.widget.AdapterView;
-
-public interface OnDataSelectionListener {
-    public void onDataSelected(AdapterView parent, View v, int position, long id );
-}
